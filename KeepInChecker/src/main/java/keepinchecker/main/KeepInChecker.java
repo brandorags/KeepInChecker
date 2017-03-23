46ae66a78e985a1a073683e4e595c11882a00de8
Brandon Ragsdale <brandon@brandonragsdale.com>
2017-03-22 23:27:12 -0600

    created project as Gradle project

---
+++ KeepInChecker/src/main/java/keepinchecker/main/KeepInChecker.java
@@ -0,0 +1,71 @@
+package keepinchecker.main;
+
+import java.io.File;
+import java.net.InetAddress;
+
+import org.pcap4j.core.PcapHandle;
+import org.pcap4j.core.PcapNetworkInterface;
+import org.pcap4j.core.PcapNetworkInterface.PromiscuousMode;
+import org.pcap4j.core.Pcaps;
+import org.pcap4j.packet.Packet;
+
+import com.sun.javafx.PlatformUtil;
+
+import keepinchecker.contstants.Contstants;
+import keepinchecker.database.DbSession;
+
+public class KeepInChecker {
+	
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello, world!");
+		initializeDatabaseConnection();
+		
+		// delete me!
+		InetAddress address = InetAddress.getByName("192.168.0.6");
+		PcapNetworkInterface networkInterface = Pcaps.getDevByAddress(address);
+		System.out.println(networkInterface.getName());
+		int snapLen = 65536;
+		PromiscuousMode mode = PromiscuousMode.PROMISCUOUS;
+		int timeout = 10000;
+		PcapHandle handle = networkInterface.openLive(snapLen, mode, timeout);
+		Packet packet = handle.getNextPacketEx();
+		handle.close();
+		System.out.println(packet.getPayload());
+	}
+	
+	private static void initializeDatabaseConnection() throws Exception {
+		Contstants.databasePath = getDatabasePath();
+		DbSession dbSession = new DbSession(Contstants.databasePath, false);
+		dbSession.createTablesIfNoneExist();
+		dbSession.commit();
+		dbSession.close();
+	}
+	
+	private static String getDatabasePath() throws Exception {
+		String databasePath = "";
+
+		if (PlatformUtil.isMac() || PlatformUtil.isLinux()) {
+			databasePath = "jdbc:sqlite:/usr/local/.KeepInChecker/KeepInChecker.sqlite";
+			if (!(new File("/usr/local/.KeepInChecker").exists())) {
+				File keepInCheckerDir = new File("/usr/local/.KeepInChecker");
+				boolean dirCreationSuccess = keepInCheckerDir.mkdirs();
+				if (!dirCreationSuccess) {
+					throw new Exception("Couldn't create directory. This application must be run by sudo.");
+				}
+			}
+		} else if (PlatformUtil.isWindows()) {
+			databasePath = "jdbc:sqlite:C:\\KeepInChecker\\KeepInChecker.sqlite";
+			if (!(new File("C:\\KeepInChecker").exists())) {
+				File keepInCheckerDir = new File("C:\\KeepInChecker");
+				keepInCheckerDir.mkdirs();
+				boolean dirCreationSuccess = keepInCheckerDir.mkdirs();
+				if (!dirCreationSuccess) {
+					throw new Exception("Couldn't create directory. This application must be run by Administrator.");					
+				}
+			}
+		}
+		
+		return databasePath;
+	}
+
+}
