cb37f96dc1ef822d6b53f6bc6c7df3fa04ba5051
Brandon Ragsdale <brandon@brandonragsdale.com>
2017-03-28 23:18:14 -0600

    cleaned up constants

--- KeepInChecker/src/main/java/keepinchecker/main/KeepInChecker.java
+++ KeepInChecker/src/main/java/keepinchecker/main/KeepInChecker.java
@@ -1,54 +1,19 @@
 package keepinchecker.main;
 
-import java.io.File;
-
-import com.sun.javafx.PlatformUtil;
-
 import keepinchecker.contstants.Contstants;
 import keepinchecker.database.DbSession;
-import keepinchecker.utility.EmailUtilities;
 
 public class KeepInChecker {
 	
 	public static void main(String[] args) throws Exception {
-		System.out.println("Hello, world!");
 		initializeDatabaseConnection();
-		EmailUtilities.sendScheduledEmail();
 	}
 	
 	private static void initializeDatabaseConnection() throws Exception {
-		Contstants.databasePath = getDatabasePath();
-		DbSession dbSession = new DbSession(Contstants.databasePath, false);
+		DbSession dbSession = new DbSession(Contstants.DATABASE_PATH, false);
 		dbSession.createTablesIfNoneExist();
 		dbSession.commit();
 		dbSession.close();
-	}
-	
-	private static String getDatabasePath() throws Exception {
-		String databasePath = "";
-
-		if (PlatformUtil.isMac() || PlatformUtil.isLinux()) {
-			databasePath = "jdbc:sqlite:/usr/local/.KeepInChecker/KeepInChecker.sqlite";
-			if (!(new File("/usr/local/.KeepInChecker").exists())) {
-				File keepInCheckerDir = new File("/usr/local/.KeepInChecker");
-				boolean dirCreationSuccess = keepInCheckerDir.mkdirs();
-				if (!dirCreationSuccess) {
-					throw new Exception("Couldn't create directory. This application must be run by sudo.");
-				}
-			}
-		} else if (PlatformUtil.isWindows()) {
-			databasePath = "jdbc:sqlite:C:\\KeepInChecker\\KeepInChecker.sqlite";
-			if (!(new File("C:\\KeepInChecker").exists())) {
-				File keepInCheckerDir = new File("C:\\KeepInChecker");
-				keepInCheckerDir.mkdirs();
-				boolean dirCreationSuccess = keepInCheckerDir.mkdirs();
-				if (!dirCreationSuccess) {
-					throw new Exception("Couldn't create directory. This application must be run by Administrator.");					
-				}
-			}
-		}
-		
-		return databasePath;
 	}
 
 }
