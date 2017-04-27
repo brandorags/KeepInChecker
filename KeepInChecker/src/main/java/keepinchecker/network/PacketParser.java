8a9efd2d3af5764a716268ed4cff582aeceed877
brandorags <brandorags@users.noreply.github.com>
2017-04-27 00:02:24 -0600

    successful parsing of important packet data

--- KeepInChecker/src/main/java/keepinchecker/network/PacketParser.java
+++ KeepInChecker/src/main/java/keepinchecker/network/PacketParser.java
@@ -2,22 +2,44 @@ package keepinchecker.network;
 
 import javax.xml.bind.DatatypeConverter;
 
+import org.apache.commons.lang3.StringUtils;
 import org.pcap4j.packet.Packet;
 import org.pcap4j.util.ByteArrays;
 
 public class PacketParser {
-	
+
 	public static final String GET = "GET";
+	public static final String HOST = "Host";
+	public static final String REFERER = "Referer";
 
 	public static String parse(String keyword, String packet) {
-		return "";
+		String value = "";
+
+		if (!StringUtils.contains(packet, keyword)) {
+			return "";
+		}
+
+		String[] packetSplitOnNewline = packet.split("\n");
+		for (String line : packetSplitOnNewline) {
+			if (StringUtils.equals(keyword, GET) && StringUtils.contains(line, keyword)) {
+				value = StringUtils.substringBefore(line.split(keyword)[1], "HTTP");
+				break;
+			} else if ((StringUtils.contains(line, HOST) || 
+					StringUtils.contains(line, REFERER)) && 
+					StringUtils.contains(line, keyword)) {
+				value = line.split(keyword + ":")[1];
+				break;
+			}
+		}
+
+		return value.trim();
 	}
-	
+
 	public static String convertToHumanReadableFormat(Packet packet) {
 		String hexString = ByteArrays.toHexString(packet.getRawData(), "");
 		byte[] hexBinary = DatatypeConverter.parseHexBinary(hexString);
-		
+
 		return new String(hexBinary);
 	}
-	
+
 }
